de6bff8b898eb0ceb6e64589ec7e92b9a7e4da42
AcilKh <acilpro@outlook.fr>
2023-01-26 20:26:48 +0100

    add modif in DataEquipementsPannes.py

--- src/main/DataEquipementsPannes.py
+++ src/main/DataEquipementsPannes.py
@@ -1,15 +1,15 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 
-spark_conf =  SparkConf().setAppName("READ-EQUIPEMENTS-PANNES").setMaster("spark://192.168.1.15:7077").set("spark.submit.deployMode", "client")
+spark_conf = SparkConf().setAppName("READ-EQUIPEMENTS-PANNES").setMaster("local[1]")
 sc = SparkContext(spark_conf)
 # Création de la session Spark
-spark = SparkSession.builder.appName("CSVtoHDFS").getOrCreate()
+spark = SparkSession.builder.config(spark_conf).appName("CSVtoHDFS").getOrCreate()
 print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 # Lecture du fichier CSV local en tant que DataFrame
-        #df = spark.read.csv("data.csv", header=True, inferSchema=True)
+df = spark.read.csv("data.csv", header=True, inferSchema=True)
 # Écriture du DataFrame sur HDFS
-        #df.write.csv("hdfs://192.168.1.2:9000/bronze/data_acil_steph", header=True)
+df.write.csv("hdfs://192.168.1.2:9000/bronze/data_acil_steph", header=True)
 # Arrêt de la session Spark
 spark.stop()
 #
